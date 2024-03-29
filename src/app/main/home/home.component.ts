9916f6daf233d8c1f651c523891f46a42b2e81fd
Fahlevi Afghani <fahleviafghani21@gmail.com>
2024-03-29 13:46:26 +0700

    feat : add project & service

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import Typed from 'typed.js';
 import { AboutComponent } from '../about/about.component';
+import { ServiceComponent } from '../service/service.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [AboutComponent],
+  imports: [
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent,
+    ProjectsComponent,
+  ],
 })
 export class HomeComponent implements OnInit, OnDestroy {
   typed: Typed;
--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -2,13 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-service',
-  templateUrl: './service.component.html',
-  styleUrls: ['./service.component.css'],
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
   standalone: true,
   imports: [CommonModule],
 })
-export class ServiceComponent implements OnInit {
+export class ProjectsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
